c278f692a1fc76b81e76a94b72bafbd8835e2914
Xavier Shay <xavier@rhnh.net>
2015-02-07 15:33:41 -0800

    Speed up yoshi's exit.

--- yi-skipbonus.asm
+++ yi-skipbonus.asm
@@ -1,16 +1,20 @@
 ; # Super Mario World 2: Yoshi's Island
 ;
-; Skips those damn bonus games.
+; Skips those damn bonus games, and speeds up the goal phase.
 
 ; This was BEQ which jumped if a flower was not landed on.
 ; Changing to BRA so it always branches, skipping bonus game.
 org $02A818
 BRA $1E
 
-; Speed up end-level flower select. Original is #$6000.
-; TODO: The yoshis still stand around awkwardly. Need to find timer for that.
+; No pause on end-level flower select. Original is #$6000.
 org $02A5E6
-LDA #$1000
+LDA #$0000
+
+; No pause before yoshi leaving the screen.
+; Original is #$0180, which if you speed up flower select feels *really* long.
+org $02ABF0
+LDA #$0000
 
 ; This was BPL which jumped over the bonus game setup if it wasn't needed.
 ; Changing to BRA so it always branches regardless of bonus settings.
